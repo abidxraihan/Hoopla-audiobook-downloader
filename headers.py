98898155093c49e64370540909e548ae9f658579
Abid Raihan <59921670+abidxraihan@users.noreply.github.com>
2025-01-24 20:18:12 +0600

    Delete headers.py

--- headers.py
+++
@@ -1,7 +0,0 @@
-headers = {
-            "license": "https://lic.drmtoday.com/license-proxy-widevine/cenc/?specConform=true",
-            "dt-custom-data": "eyJ1c2VySWQiOjEzMzEwNjQxLCJzZXNzaW9uSWQiOiIzNzg4Njg3OTEiLCJtZXJjaGFudCI6Imhvb3BsYSJ9",
-            "pssh": "AAAAOHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABgSECskZ6dAErCGWXixCHqZSx1I49yVmwY=",
-            "buildInfo": "",
-            "proxy": "",
-        }
